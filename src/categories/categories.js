78a91359f7d2c54459828a393ec60778ab62a028
Daniel Pino <dapino@outlook.com>
2018-09-28 16:25:17 -0500

    Crea formulario de búsqueda

--- src/categories/categories.js
+++ src/categories/categories.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Category from "./category"
 import './categories.css'
+import Search from "../widgets/components/search";
 
 const Categories = props => (
     <div className="Categories">
+        <Search/>
         {
             props.categories.map( item => (
                 <Category
---
+++ src/widgets/components/search.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './search.css'
+
+const Search = () => (
+    <form action="" className="Search">
+        <input
+            type="text"
+            placeholder="Buscador"
+            className="Search-input"
+        />
+    </form>
+)
+
+export default Search
