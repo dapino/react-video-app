ad4e04f57d04cb6e7851824ecddd04588b091383
Daniel Pino <dapino@outlook.com>
2018-10-03 17:20:37 -0500

    WIP: estados del video

--- src/player/components/video.js
+++ src/player/components/video.js
@@ -2,12 +2,29 @@ import React, { Component } from 'react'
 import './video.css'
 
 class Video extends Component {
+    togglePlay() {
+        if (this.props.pause) {
+            this.video.play()
+        } else {
+            this.video.pause()
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.pause !== this.props.pause) {
+            this.togglePlay()
+        }
+    }
+    setRef = e => {
+        this.video = e
+    }
     render () {
         return (
             <div className="Video">
                 <video
                     autoPlay={this.props.autoplay}
                     src={this.props.src}
+                    ref={this.setRef}
                 />
             </div>
         )
